68415092e77777a8af75a7aa477e098a6f7d4a24
ainsleyskye <ndboyd95@gmail.com>
2022-06-17 11:21:48 -0600

    part 1 work

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Your code here
+const table = document.getElementsByTagName(`table`)[0];
+
+function makeRow() {
+  const row = document.createElement(`tr`);
+  for (let i = 0; i < 5; i++) {
+    const td = document.createElement(`td`);
+    row.appendChild(td);
+  }
+  table.appendChild(row);
+}
+
+makeRow();
+makeRow();
+makeRow();
+
+const addButton = document.getElementById(`add-row`);
+addButton.addEventListener(`click`, makeRow);
